71b6f0146309ccfe0e9d57f0fca9a9ffdcce2e76
stephanofer <120071974+stephanofer@users.noreply.github.com>
2025-04-10 23:46:49 -0500

    feat: update Astro configuration and add new assets
    - Enhanced `astro.config.mjs` with image service configuration and improved environment variable handling.
    - Added new font file `Seven.woff2` to the project.
    - Created `Add.astro` and `Sword.astro` components for SVG icons.
    - Introduced `Thophy.svg` and `Trophy.astro` for trophy icons.
    - Implemented `Kill.astro` icon for leaderboard functionality.
    - Updated `LeaderBoards.astro` to utilize new trophy icons and display player images.
    - Modified `MainHead.astro` to preload the new font.
    - Corrected display text for KOTH in ranking links.
    - Added new font-face declaration for "Seven" in global styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,32 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig, envField, passthroughImageService } from "astro/config";
 
 import node from "@astrojs/node";
-
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  // image: {
+  //   domains: ["render.crafty.gg"],
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "**.crafty.gg",
+  //     },
+  //   ],
+  // },
   adapter: node({
     mode: "standalone",
   }),
   env: {
     schema: {
-      API_RPG: envField.string({context: "server", access: "secret" }),
-      API_SURVI: envField.string({context: "server", access: "secret" }),
-      API_PLAYERS_RPG: envField.string({context: "server", access: "secret" }),
-      API_PLAYERS_SURVI: envField.string({context: "server", access: "secret" }),
+      API_RPG: envField.string({ context: "server", access: "secret" }),
+      API_SURVI: envField.string({ context: "server", access: "secret" }),
+      API_PLAYERS_RPG: envField.string({ context: "server", access: "secret" }),
+      API_PLAYERS_SURVI: envField.string({
+        context: "server",
+        access: "secret",
+      }),
     },
   },
 });
